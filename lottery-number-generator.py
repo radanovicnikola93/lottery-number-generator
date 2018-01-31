b2b8ed585a4d8de4d1b1ecd16d3355ee89bd15b7
Nikola <radanovicnikola93@gmail.com>
2018-01-31 19:19:45 +0100

    Added .sort to numbers_l to sort numbers after running, Added main() function with the repeat question

--- lottery-number-generator.py
+++ lottery-number-generator.py
@@ -1,8 +1,5 @@
 import random
 
-print "Welcome to the Lottery numbers generator."
-
-
 
 def numbers_generator(quantity):
     numbers_l = []
@@ -13,7 +10,28 @@ def numbers_generator(quantity):
 
         lottery_numbers = random.randint(1, 42)
 
-        return numbers_l
+        if lottery_numbers not in numbers_l:
+            numbers_l.append(lottery_numbers)
+            numbers_l.sort()
+
+    return numbers_l
 
 
 
+def main():
+    print "***LOTTERY NUMBER GENERATOR***"
+
+    while True:
+        user_input_quantity = raw_input('How many random lottery numbers would you have? ')
+
+
+        quantity_number = int(user_input_quantity)
+        print numbers_generator(quantity_number)
+
+        again = raw_input('Would you like to run the program again? (y/n) ')
+
+        if again != 'y' and again != 'yes':
+            break
+
+if __name__ == '__main__':
+    main()
